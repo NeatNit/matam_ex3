819766668e07286eeb5f6e278df874af25e78b0c
ronm100 <57960026+ronm100@users.noreply.github.com>
2020-01-07 23:06:54 +0200

    Vehicle interface added

--- ParkingLot.h
+++ ParkingLot.h
@@ -4,6 +4,7 @@
 #include "ParkingLotTypes.h"
 #include "Time.h"
 #include "ParkingSpot.h"
+#include "UniqueArray.h"
 
 namespace MtmParkingLot {
 
@@ -11,6 +12,10 @@ namespace MtmParkingLot {
     using std::ostream;
 
     class ParkingLot {
+    private:
+        UniqueArray vehicles[];
+        UniqueArray free_spots[];
+        UniqueArray taken_spots[];
     public:
 
         ParkingLot(unsigned int parkingBlockSizes[]);
@@ -20,6 +25,23 @@ namespace MtmParkingLot {
         ParkingResult getParkingSpot(LicensePlate licensePlate, ParkingSpot& parkingSpot) const;
         void inspectParkingLot(Time inspectionTime);
         friend ostream& operator<<(ostream& os, const ParkingLot& parkingLot);
+
+    };
+
+    class Vehicle{
+    private:
+        Time arrival_time;
+        VehicleType type;
+        LicensePlate plate;
+        int debt;
+        ParkingSpot spot;
+        int calculateDebt(Time departure, VehicleType type);
+    public:
+        VehicleType (Time arrival_time, VehicleType type, LicensePlate plate, ParkingSpot spot);
+        ~Vehicle();
+        void addDebt(Time departure);
+        void giveTicket();
+
 
     };
 
