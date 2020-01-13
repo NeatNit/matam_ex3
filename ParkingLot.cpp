e8870989fe0d2701c74ec7947cda48266a76dfa0
NeatNit <neatnit@gmail.com>
2020-01-13 22:55:31 +0200

    most of enterParking

--- ParkingLot.cpp
+++ ParkingLot.cpp
@@ -1,5 +1,6 @@
 #include "ParkingLot.h"
 #include "ParkingLotTypes.h"
+#include "ParkingLotPrinter.h"
 
 namespace MtmParkingLot {
     using std::cout;
@@ -19,16 +20,30 @@ namespace MtmParkingLot {
 
     ParkingResult ParkingLot::enterParking(VehicleType vehicleType,
         LicensePlate licensePlate, Time entranceTime) {
-
-        UniqueArray<LicensePlate> parking_block& = Motorbikes;
-        if (type == CAR) {
-            parking_block = Cars;
-        } else if (type == HANDICAPPED) {
-            parking_block = Handicaps;
+        if (AllVehicles.count(licensePlate)) {
+            Vehicle vehicle = AllVehicles[licensePlate];
+            cout << vehicle;
+            ParkingLotPrinter::printEntryFailureAlreadyParked(cout,
+                vehicle.getParkingSpot());
+            return VEHICLE_ALREADY_PARKED;
         }
 
-        if (parking_block.getCount() == parking_block.getSize()) {
-            return NO_ROOM;
+        VehicleType parking_block_type = vehicleType;
+        UniqueArray<LicensePlate> *parking_block =
+            ParkingBlocks[parking_block_type];
+
+        if (vehicleType == VehicleType::HANDICAPPED
+            && parking_block->getCount() == parking_block->getSize()) {
+            // handicapped full, fallback to normal parking
+            parking_block_type = VehicleType::CAR;
+            parking_block = &ParkingBlocks[parking_block_type];
+        }
+
+        if (parking_block->getCount() == parking_block->getSize()) {
+            ParkingLotPrinter::printVehicle(cout, vehicleType, licensePlate,
+                entranceTime);
+            ParkingLotPrinter::printEntryFailureNoSpot(cout);
+            return NO_EMPTY_SPOT;
         }
 
         unsigned int parking_number = parking_block.insert(licensePlate);
