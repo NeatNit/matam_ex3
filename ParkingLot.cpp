da0a3a557e6412f1dcedffb6edca9ce395b11950
NeatNit <neatnit@gmail.com>
2020-01-14 00:03:03 +0200

    line length & junk

--- ParkingLot.cpp
+++ ParkingLot.cpp
@@ -10,8 +10,7 @@ namespace MtmParkingLot {
 
     ParkingLot::ParkingLot(unsigned int parkingBlockSizes[]) {
         for (int i = VehicleType::FIRST; i <= VehicleType::LAST; ++i) {
-            parkingBlocks[i] =
-            new ParkingBlock(parkingBlockSizes[i]);
+            parkingBlocks[i] = new ParkingBlock(parkingBlockSizes[i]);
         }
     }
 
@@ -24,7 +23,8 @@ namespace MtmParkingLot {
     ParkingResult ParkingLot::enterParking(VehicleType vehicleType,
         LicensePlate licensePlate, Time entranceTime) {
         if (AllVehicles.count(licensePlate)) {
-            const ParkedVehicle& vehicle = AllVehicles.find(licensePlate)->second;
+            const ParkedVehicle& vehicle =
+                AllVehicles.find(licensePlate)->second;
             cout << vehicle;
             ParkingLotPrinter::printEntryFailureAlreadyParked(cout,
                 vehicle.getParkingSpot());
@@ -32,12 +32,12 @@ namespace MtmParkingLot {
         }
 
         VehicleType parking_block_type = vehicleType;
-        ParkingBlock *parking_block =
+        ParkingBlock * parking_block =
             parkingBlocks[parking_block_type];
 
         if (vehicleType == VehicleType::HANDICAPPED
             && parking_block->getCount() == parking_block->getSize()) {
-            // handicapped full, fallback to normal parking
+            // handicapped lot is full, fallback to normal parking
             parking_block_type = VehicleType::CAR;
             parking_block = parkingBlocks[parking_block_type];
         }
@@ -55,8 +55,9 @@ namespace MtmParkingLot {
         ParkingSpot spot(parking_block_type, parking_number);
 
         AllVehicles.emplace(std::piecewise_construct,
-              std::forward_as_tuple(licensePlate),
-              std::forward_as_tuple(licensePlate, vehicleType, spot, entranceTime));
+            std::forward_as_tuple(licensePlate),
+            std::forward_as_tuple(licensePlate, vehicleType, spot,
+                entranceTime));
 
         return SUCCESS;
     }
