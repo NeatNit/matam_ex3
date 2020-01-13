fe80514768017e2ffc6b31b3a85e6e109e7631ff
NeatNit <neatnit@gmail.com>
2020-01-13 14:01:51 +0200

    demonstration code for ParkingLot using map and stuff

---
+++ ParkingLot.cpp
@@ -0,0 +1,55 @@
+#include "ParkingLot.h"
+
+namespace MtmParkingLot {
+    ParkingLot::ParkingLot(unsigned int parkingBlockSizes[]) {
+
+    }
+
+    ParkingResult ParkingLot::getParkingSpot(LicensePlate licensePlate,
+        ParkingSpot& parkingSpot) const {
+        if (!AllVehicles.count(licensePlate)) {
+            // car is not in the parking lot
+            return "some kind of error";
+        }
+        Vehicle vehicle = AllVehicles[licensePlate];
+
+        parkingSpot = ParkingSpot(vehicle.type, vehicle.parkingSpot);
+        return SUCCESS;
+
+        /*
+        // select according to type
+        UniqueArray<LicensePlate> parking_block& = Motorbikes;
+        if (type == CAR) {
+            parking_block = Cars;
+        } else if (type == HANDICAPPED) {
+            parking_block = Handicaps;
+        }
+
+        unsigned int parking_number = parking_block.getIndex(licensePlate);
+
+        parkingSpot = ParkingSpot(type, parking_number);
+        return SUCCESS;
+        */
+    }
+
+    ParkingResult ParkingLot::enterParking(VehicleType vehicleType,
+        LicensePlate licensePlate, Time entranceTime) {
+
+        UniqueArray<LicensePlate> parking_block& = Motorbikes;
+        if (type == CAR) {
+            parking_block = Cars;
+        } else if (type == HANDICAPPED) {
+            parking_block = Handicaps;
+        }
+
+        if (parking_block.getCount() == parking_block.getSize()) {
+            return NO_ROOM;
+        }
+
+        unsigned int parking_number = parking_block.insert(licensePlate);
+
+        AllVehicles[licensePlate] = Vehicle(licensePlate, type, parking_number, entranceTime);
+
+        return SUCCESS;
+    }
+}
--- ParkingLot.h
+++ ParkingLot.h
@@ -5,12 +5,24 @@
 #include "Time.h"
 #include "ParkingSpot.h"
 
+class Vehicle {
+public:
+    LicensePlate licensePlate;
+    VehicleType type;
+    unsigned int parkingSpot;
+    Time entranceTime;
+}
+
 namespace MtmParkingLot {
 
     using namespace ParkingLotUtils;
     using std::ostream;
 
     class ParkingLot {
+        UniqueArray<LicensePlate> Motorbikes;
+        UniqueArray<LicensePlate> Handicaps;
+        UniqueArray<LicensePlate> Cars;
+        std::map<LicensePlate, Vehicle> AllVehicles;
     public:
 
         ParkingLot(unsigned int parkingBlockSizes[]);
